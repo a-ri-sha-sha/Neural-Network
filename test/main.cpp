f46b1eaf6933ae283e38edf545c587c6acbd89bf
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2024-05-14 19:56:16 +0300

    Problems with Cmake

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,16 @@
-#include "test.h"
-#include "../Network/Except.h"
+//#include "test.h"
+//#include "../Network/Except.h"
+#include <iostream>
+#include <functional>
+
+using Fun = std::function<void()>;
 
 int main() {
-    try {
-        mnist_test::RunAllTest();
-    } catch (...) {
-        except::react();
-    }
+//    try {
+//        mnist_test::RunAllTest();
+//    } catch (...) {
+//        except::react();
+//    }
+    std::cout << "Hello world\n";
     return 0;
 }
